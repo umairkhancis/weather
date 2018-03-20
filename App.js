2e8ba01c3cfe47085f28a47a0ce4844d6f1629a5
umair.khan <umair.khan>
2018-03-21 00:01:46 +0400

    final commit

--- App.js
+++ App.js
@@ -7,18 +7,90 @@ import {
 } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons'
 import {fetchWeather} from './weatherAPI.js'
+import Highlighter from 'react-native-highlight-words'
+
+const iconNames = {
+  Default: 'md-time',
+  Clear: 'md-sunny',
+  Rain: 'md-rainy',
+  Thunderstorm: 'md-thunderstorm',
+  Clouds: 'md-cloudy',
+  Snow: 'md-snow',
+  Drizzle: 'md-umbrella'
+}
+
+const phrases = {
+  Default: {
+    title: "Fetching the Awesome Weather Data",
+    subtitle: "Be patient, you are witnessing a miracle!",
+    highlight: "Awesome",
+    color: "#636363",
+    backgroundColor: "#9C9C9C"
+  },
+  Clear: {
+    title: "It's Awesome Amaze Balls",
+    subtitle: "Let's play cricket",
+    highlight: "Awesome",
+    color: "#E32500",
+    backgroundColor: "#FFD017"
+  },
+  Rain: {
+    title: "Go Rain Go",
+    subtitle: "Let's play football",
+    highlight: "Rain",
+    color: "#004A96",
+    backgroundColor: "#2F343A"
+  },
+  Thunderstorm: {
+    title: "Oh! That's Scary",
+    subtitle: "Unplug those devices",
+    highlight: "Scary",
+    color: "#FBFF46",
+    backgroundColor: "#020202"
+  },
+  Clouds: {
+    title: "Cloud Storage Limit Reached",
+    subtitle: "error: 5000 - cirrocumulus",
+    highlight: "Limit",
+    color: "#0044FF",
+    backgroundColor: "#939393"
+  },
+  Snow: {
+    title: "Snowy Please Go Away",
+    subtitle: "Let's code all day",
+    highlight: "Please",
+    color: "#021D4C",
+    backgroundColor: "#15A678"
+  },
+  Drizzle: {
+    title: "Hey! Look Outside",
+    subtitle: "Let's go for a walk",
+    highlight: "Look",
+    color: "#B3F6E4",
+    backgroundColor: "#1FBB68"
+  }
+}
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      temp: 0,
+      weather: 'Default'
+    }
+  }
 
   componentDidMount() {
     this.getLocation();
-    fetchWeather(24, 67)
-      .then(resp => console.log(resp.main));
   }
 
   getLocation() {
       navigator.geolocation.getCurrentPosition(
-        (posData) => console.log(posData),
+        (posData) => fetchWeather(posData.coords.latitude, posData.coords.longitude)
+                      .then(res => this.setState({
+                        temp: Math.round(res.temp),
+                        weather: res.weather
+                      })),
         (error) => alert(error),
         {timeout: 10000}
       )
@@ -26,18 +98,21 @@ export default class App extends Component {
 
   render() {
     return (
-    <View style={styles.container}>
+    <View style={[styles.container, {backgroundColor: phrases[this.state.weather].backgroundColor}]}>
         <StatusBar hidden={true} />
         <View style={styles.header}>
-          <Icon name={'ios-sunny'} size={80} color={'white'} /> 
-          <Text style={styles.temp}>24°</Text>
+          <Icon name={iconNames[this.state.weather]} size={80} color={'white'} /> 
+          <Text style={styles.temp}>{this.state.temp}°</Text>
         </View>
 
         <View style={styles.body}>
-          <Text style={styles.title}>
-            Build an <Text style={{color: 'red'}}> Awesome </Text> Weather App
-          </Text>
-          <Text style={styles.subtitle}>Let's Make a Rain</Text>
+          <Highlighter
+            style={styles.title}
+            highlightStyle={{color: phrases[this.state.weather].color}}
+            searchWords={[phrases[this.state.weather].highlight]}
+            textToHighlight={phrases[this.state.weather].title}
+          />
+          <Text style={styles.subtitle}>{phrases[this.state.weather].subtitle}</Text>
         </View>
       </View>            
     );
--- weatherAPI.js
+++ weatherAPI.js
@@ -1,11 +1,11 @@
 const rootUrl = 'http://samples.openweathermap.org/data/2.5/weather?appid=511c025d9660f85edada762f6f115893';
 
 export const fetchWeather = (lat, lon) => {
-	const url = rootUrl += '&lat'+lat+'&lon'+lon
+	const url = rootUrl += '&lat='+lat+'&lon='+lon
 	return fetch(url)
 		.then(res => res.json())
 		.then(json => ({
-			temp: json.main.temp,
-			weather: json.weather[0].main
-		}))
+			'temp': json.main.temp,
+			'weather': json.weather[0].main
+		}) )
 }
